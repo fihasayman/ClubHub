090a7c40628f383fd8d92c76503425f642242d46
Safiha Ayman <safihayman02@gmail.com>
2025-12-24 23:11:19 +0600

    Safiha

--- Final Project/Welcome.Designer.cs
+++ Final Project/Welcome.Designer.cs
@@ -29,43 +29,31 @@
         private void InitializeComponent()
         {
             button2 = new Button();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button2
             // 
-            button2.Location = new Point(442, 168);
+            button2.Location = new Point(272, 117);
             button2.Name = "button2";
             button2.Size = new Size(160, 120);
             button2.TabIndex = 1;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(275, 177);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(125, 62);
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
-            // 
             // Welcome
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Green;
+            BackColor = Color.FromArgb(13, 34, 21);
             ClientSize = new Size(800, 450);
-            Controls.Add(pictureBox1);
             Controls.Add(button2);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Welcome";
             Text = "Welcome";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
         private Button button2;
-        private PictureBox pictureBox1;
     }
 }
